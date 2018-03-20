6c3deb70ffc85ca6f9989d360fa3302174dd31b2
Christina Huang <christinahuang@nat-oitwireless-inside-vapornet100-d-9512.Princeton.EDU>
2018-03-20 14:20:01 -0400

    removing constants

--- FFChallenge_v4/constantProcess.py
+++ FFChallenge_v4/constantProcess.py
@@ -1,9 +1,13 @@
 import pandas as pd
 df = pd.read_csv('output.csv', header=0, low_memory=False)
-
+f = df.columns.tolist()
+print len(f)
 with open('constantVariables.txt', 'r') as file:
     for line in file:
-        word = line.split()[0]
-        df.drop(word, 1)
-
-df.to_csv('noConstants.csv', index=False)
+        word = unicode(line.split()[0], "utf-8")
+        f.remove(word)
+	#print len(f)
+#	df.drop(word, 1)
+keep = df[f]
+print len(keep.columns)
+keep.to_csv('noConstants.csv', index=False)
